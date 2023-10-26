b6548cb93e542d1d7dce96d3cb2ca8d9774df600
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-10-26 18:34:50 +0200

    feat: setup vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import Icons from "unplugin-icons/vite";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://choose-tech.com",
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
   vite: {
-    plugins: [Icons({ compiler: "astro" })],
+    plugins: [Icons({
+      compiler: "astro"
+    })]
   },
+  output: "hybrid",
+  adapter: vercel()
 });
